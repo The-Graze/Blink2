66fa8cdf4645f213a7e43a61ef6cb9ebaf043b5f
Graze <Graze>
2024-10-16 20:10:25 +0100

    ver

--- Blink2/PluginInfo.cs
+++ Blink2/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "com.graze.gorillatag.blink2";
         public const string Name = "Blink2";
-        public const string Version = "1.0.1";
+        public const string Version = "1.0.2";
     }
 }
